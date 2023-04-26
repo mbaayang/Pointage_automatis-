b9f55c22221c1648b01c34c7a5417411f3ade992
mbaayang <mbayangueye07@gmail.com>
2023-04-26 17:03:02 +0000

    header

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Dashboard from './Components/Dashboard/Dashboard.tsx'
+import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom'
+import PageAdmin from '../Pages/PageAdmin.tsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/admin" element={<PageAdmin />} >
+
+      </Route>
+      <Route path="*" element={<div>Not found</div>} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-   {/*  <App /> */}
-   <Dashboard></Dashboard>
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
