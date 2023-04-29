030bcda77c02ee1c8920ef1b6b32a5aff9411dd8
MHDLamine <mouhamed.ngom50@gmail.com>
2023-04-29 18:51:42 +0000

    ok

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,18 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-
-
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { databaseProviders } from "./database/database.providers";
 
 const entities = [];
 
 @Module({
-  imports: [ ConfigModule.forRoot({
-    isGlobal: true,
-  }),
- ],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ...databaseProviders],
 })
 export class AppModule {}
--- Backend/src/app.service.ts
+++ Backend/src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Connection } from 'mysql2/promise';
 
 @Injectable()
+
 export class AppService {
-  getHello(): string {
+  constructor(@Inject('DATABASE_CONNECTION') private connection: Connection) {}
+/*   getHello(): string {
     return 'Hello World!';
+  } */
+  async getHello(): Promise<string> {
+    const [rows, fields] = await this.connection.execute('SELECT * FROM users');
+    return `Hello World! Users: ${JSON.stringify(rows)}`;
   }
 }
