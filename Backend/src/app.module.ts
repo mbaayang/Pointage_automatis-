cd19e8725d57f302b5b349ccdbea1a51a6cccc70
MHDLamine <mouhamed.ngom50@gmail.com>
2023-04-29 18:35:49 +0000

    pull

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+
+
+const entities = [];
 
 @Module({
-  imports: [],
+  imports: [ ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+ ],
   controllers: [AppController],
   providers: [AppService],
 })
