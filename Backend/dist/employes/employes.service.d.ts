a09dc1c8b211449f1003c1ac4822a0cb46aaf763
MHDLamine <mouhamed.ngom50@gmail.com>
2023-05-03 15:00:01 +0000

    send

--- Backend/dist/employes/employes.service.d.ts
+++ Backend/dist/employes/employes.service.d.ts
@@ -1,7 +1,8 @@
-import { Repository } from 'typeorm';
-import { CreateEmployeDto } from './dto/create-employe.dto';
-import { UpdateEmployeDto } from './dto/update-employe.dto';
-import { Employes } from './entities/employe.entity';
+import { Repository } from "typeorm";
+import { CreateEmployeDto } from "./dto/create-employe.dto";
+import { UpdateEmployeDto } from "./dto/update-employe.dto";
+import { UpdatePasswordDto } from "./dto/updatePassword.dto";
+import { Employes } from "./entities/employe.entity";
 export declare class EmployesService {
     private employesRepository;
     constructor(employesRepository: Repository<Employes>);
@@ -14,8 +15,9 @@ export declare class EmployesService {
         role: string;
         etat: boolean;
     } & Employes>;
-    findAll(): string;
-    findOne(id: number): string;
-    update(id: number, updateEmployeDto: UpdateEmployeDto): string;
-    remove(id: number): string;
+    findAll(): Promise<Employes[]>;
+    findOne(id: number): Promise<Employes>;
+    update(id: number, updateEmployeDto: UpdateEmployeDto): Promise<import("typeorm").UpdateResult>;
+    remove(id: number): Promise<void>;
+    updatePassword(email: string, updatePassword: UpdatePasswordDto): Promise<any>;
 }
--- Backend/src/employes/employes.controller.ts
+++ Backend/src/employes/employes.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { EmployesService } from './employes.service';
-import { CreateEmployeDto } from './dto/create-employe.dto';
-import { UpdateEmployeDto } from './dto/update-employe.dto';
-
-@Controller('employes')
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { EmployesService } from "./employes.service";
+import { CreateEmployeDto } from "./dto/create-employe.dto";
+import { UpdateEmployeDto } from "./dto/update-employe.dto";
+import { UpdatePasswordDto } from "./dto/updatePassword.dto";
+@Controller("employes")
 export class EmployesController {
   constructor(private readonly employesService: EmployesService) {}
 
-  @Post('post')
+  @Post("post")
   create(@Body() createEmployeDto: CreateEmployeDto) {
     return this.employesService.create(createEmployeDto);
   }
@@ -17,18 +25,26 @@ export class EmployesController {
     return this.employesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.employesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmployeDto: UpdateEmployeDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateEmployeDto: UpdateEmployeDto) {
     return this.employesService.update(+id, updateEmployeDto);
   }
+  @Patch("password/:id")
+  async updatePassword(
+    @Param("id") email: string,
+    @Body() updatePasswordDto: UpdatePasswordDto
+    
+  ) :Promise<any | null>{
+    await this.employesService.updatePassword(email, updatePasswordDto);
+  }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.employesService.remove(+id);
   }
 }
